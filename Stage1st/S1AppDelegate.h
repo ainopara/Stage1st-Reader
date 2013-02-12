99cd1982fe4f2ab093314c93fe06210c16c2da83
Gabriel <Gal3rielol@Gmail.com>
2013-02-12 22:33:26 +0800

    Initial Commit

---
+++ Stage1st/S1AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  S1AppDelegate.h
+//  Stage1st
+//
+//  Created by Suen Gabriel on 2/12/13.
+//  Copyright (c) 2013 Renaissance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface S1AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Stage1st/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Stage1st
+//
+//  Created by Suen Gabriel on 2/12/13.
+//  Copyright (c) 2013 Renaissance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "S1AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([S1AppDelegate class]));
+    }
+}
