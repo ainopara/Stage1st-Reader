da3204743c1311157e9651f5bfa6022521c5f3f1
ainopara <ainopara@gmail.com>
2014-01-26 21:37:34 +0800

    no need to use ip address to access stage1st. and use redirect ip address will cause cache judge error. fixed.

--- Stage1st/S1AppDelegate.m
+++ Stage1st/S1AppDelegate.m
@@ -30,7 +30,7 @@
         [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"Display"];
     }
     if (![[NSUserDefaults standardUserDefaults] valueForKey:@"BaseURL"]) {
-        [[NSUserDefaults standardUserDefaults] setValue:@"http://220.196.42.172" forKey:@"BaseURL"];
+        [[NSUserDefaults standardUserDefaults] setValue:@"http://bbs.saraba1st.com" forKey:@"BaseURL"];
     }
     if (![[NSUserDefaults standardUserDefaults] valueForKey:@"FontSize"]) {
         [[NSUserDefaults standardUserDefaults] setValue:@"15px" forKey:@"FontSize"];
