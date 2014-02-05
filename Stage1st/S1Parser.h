5b50673f5bda228e81ecd705c78acbca52808baa
ainopara <ainopara@gmail.com>
2014-02-05 16:01:48 +0800

    remove codes that no more used.

--- Stage1st/S1Parser.h
+++ Stage1st/S1Parser.h
@@ -10,10 +10,7 @@
 
 @interface S1Parser : NSObject
 
-+ (NSArray *)topicsFromHTMLString:(NSString *)rawString withContext:(NSDictionary *)context;
 + (NSArray *)topicsFromHTMLData:(NSData *)rawData withContext:(NSDictionary *)context;
-
-+ (NSString *)contentsFromHTMLString:(NSMutableString *)HTMLString withOffset:(NSInteger)offset;
 + (NSString *)contentsFromHTMLData:(NSData *)rawData withOffset:(NSInteger)offset;
 
 + (NSString *)formhashFromThreadString:(NSString *)HTMLString;
