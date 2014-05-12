6f5fa2371278ee593950c078c26478e7c15f5f28
ainopara <ainopara@gmail.com>
2014-05-12 11:49:54 +0800

    add referrer header (but not work for now)

--- Stage1st/S1URLCache.m
+++ Stage1st/S1URLCache.m
@@ -16,7 +16,8 @@
     NSString *URLString = [[request URL] absoluteString];
     NSString *baseURLString = [[NSUserDefaults standardUserDefaults] valueForKey:@"BaseURL"];
     NSString *prefix = [NSString stringWithFormat:@"%@static/image/smiley", baseURLString];
-    if ([URLString hasPrefix:prefix]) {
+    
+    if ([URLString hasPrefix:prefix]) { // when request mahjong
         NSString *localPath = [[[NSBundle mainBundle] resourcePath] stringByAppendingString:@"/Mahjong"];
         NSRange range = [URLString rangeOfString:prefix];
         NSString *suffix = [URLString substringFromIndex:range.location + range.length];
@@ -37,16 +38,15 @@
             NSLog(@"smiley not cached: %@", URLString);
             return [super cachedResponseForRequest:request];
         }
-        
-        
-        
     }
-    else if ([URLString hasPrefix:baseURLString]) {
+    else if ([URLString hasPrefix:baseURLString]) { // when request webpages or attachments
         return [super cachedResponseForRequest:request];
     }
-    else {
+    else { // when request pictures from other websites
         if ([[NSUserDefaults standardUserDefaults] boolForKey:@"Display"]) {
-            return [super cachedResponseForRequest:request];
+            NSMutableURLRequest * newRequest = [request mutableCopy];
+            [newRequest addValue:@"Referer" forHTTPHeaderField:@"http://bbs.saraba1st.com/2b/thread-1003768-1-2.html"];
+            return [super cachedResponseForRequest:newRequest];
         }
         else {
             NSData *imageData = [NSData dataWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"Placeholder" ofType:@"png"]];
