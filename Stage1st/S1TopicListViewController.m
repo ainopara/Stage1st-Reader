071894506d616fe3aadc1a34e22fb371dc7bbcf2
ainopara <ainopara@gmail.com>
2015-12-11 11:59:10 +0800

    Try to resolve crash in theme change part.

--- Stage1st/S1TopicListViewController.m
+++ Stage1st/S1TopicListViewController.m
@@ -681,7 +681,10 @@ static NSString * const cellIdentifier = @"TopicCell";
     }
     self.searchBar.tintColor = [[APColorManager sharedInstance] colorForKey:@"topiclist.searchbar.tint"];
     self.searchBar.barTintColor = [[APColorManager sharedInstance] colorForKey:@"topiclist.searchbar.bartint"];
-    self.searchBar.keyboardAppearance = [[APColorManager sharedInstance] isDarkTheme] ? UIKeyboardAppearanceDark : UIKeyboardAppearanceDefault;
+    // TempFix: Fix for a crash found in a iOS7.0.4 device.
+    if ([self.searchBar respondsToSelector:@selector(setKeyboardAppearance:)]) {
+        self.searchBar.keyboardAppearance = [[APColorManager sharedInstance] isDarkTheme] ? UIKeyboardAppearanceDark : UIKeyboardAppearanceDefault;
+    }
     [self.tableView reloadData];
     [self.scrollTabBar updateColor];
     [self.navigationBar setBarTintColor:[[APColorManager sharedInstance]  colorForKey:@"appearance.navigationbar.bartint"]];
