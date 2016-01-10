c483eaf8f99b23cdd4987e5257a58b8f478c4b16
ainopara <ainopara@gmail.com>
2016-01-10 22:14:45 +0800

    remove manual calling of synchronize for user default.

--- Stage1st/S1AppDelegate.m
+++ Stage1st/S1AppDelegate.m
@@ -181,7 +181,7 @@ S1AppDelegate *MyAppDelegate;
 {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    [[NSUserDefaults standardUserDefaults] synchronize];
+    // [[NSUserDefaults standardUserDefaults] synchronize]; // This is automatically called.
     [[S1DataCenter sharedDataCenter] cleaning];
 }
 
