4382525fd2f000b05f71b09ac8590a88742deade
ainopara <ainopara@gmail.com>
2016-05-17 15:20:11 +0800

    LoginManager: Improve documentation.

--- Stage1st/LoginManager.swift
+++ Stage1st/LoginManager.swift
@@ -19,16 +19,23 @@ enum LoginProgress {
 }
 
 final class LoginManager: NSObject {
-    static let sharedInstance = {
+    static let sharedInstance = { // TODO: LoginManager should not be a singleton, it should be a property of current context
         return LoginManager()
     }()
 
-    let baseURL = "http://bbs.saraba1st.com/2b"
+    let baseURL = "http://bbs.saraba1st.com/2b" // TODO: Replace to configured by a singleton
 
+    /**
+     A check request should be sent to a discuz! server to make sure whether a seccode is necessary for login.
+
+     - parameter noSechashBlock:  Executed if seccode is disabled on this server.
+     - parameter hasSeccodeBlock: Executed if seccode is enabled on this server.
+     - parameter failureBlock:    Executed if this request failed.
+     */
     func checkLoginType(noSechashBlock noSechashBlock: () -> Void,
                                        hasSeccodeBlock: (sechash: String) -> Void,
                                        failureBlock: (error: NSError) -> Void) {
-        clearCookies()
+        logout()
         let parameters: [String: AnyObject] = ["module": "secure", "version": 1, "mobile": "no", "type": "login"]
         Alamofire.request(.GET, baseURL + "/api/mobile/index.php", parameters: parameters, encoding: .URL, headers: nil).responseJASON { (response) in
             debugPrint(response.request)
@@ -45,6 +52,16 @@ final class LoginManager: NSObject {
         }
     }
 
+    /**
+     Request to login when seccode is not necessary.
+
+     - parameter username:             Username of account.
+     - parameter password:             Password of account.
+     - parameter secureQuestionNumber: Secure question number of account. This should be set to 0 if no question is setted.
+     - parameter secureQuestionAnswer: Answer of secure question of account.
+     - parameter successBlock:         Executed if login request finished without network error.
+     - parameter failureBlock:         Executed if login request not finished due to network error.
+     */
     func login(username: String,
                password: String,
                secureQuestionNumber: Int,
@@ -68,24 +85,22 @@ final class LoginManager: NSObject {
         }
     }
 
-    func logout() {
-        clearCookies()
-        NSUserDefaults.standardUserDefaults().setObject(nil, forKey: "InLoginStateID")
-    }
-
-    func isInLogin() -> Bool {
-        let loginID: String? = NSUserDefaults.standardUserDefaults().objectForKey("InLoginStateID") as? String
-        return loginID == nil
-    }
-}
-
-extension LoginManager {
-    private func clearCookies() {
-        let cookieStorage = NSHTTPCookieStorage.sharedHTTPCookieStorage()
-        if let cookies = cookieStorage.cookies {
-            for cookie in cookies {
-                cookieStorage.deleteCookie(cookie)
+    func logout(finishBlock: () -> Void = {}) {
+        func clearCookies() {
+            let cookieStorage = NSHTTPCookieStorage.sharedHTTPCookieStorage()
+            if let cookies = cookieStorage.cookies {
+                for cookie in cookies {
+                    cookieStorage.deleteCookie(cookie)
+                }
             }
         }
+        clearCookies() // TODO: only delete cookies about this account.
+        NSUserDefaults.standardUserDefaults().setObject(nil, forKey: "InLoginStateID") // TODO: move this to finish block.
+        finishBlock()
+    }
+
+    func isInLogin() -> Bool { // TODO: check cookies rather than a global state.
+        let loginID: String? = NSUserDefaults.standardUserDefaults().objectForKey("InLoginStateID") as? String
+        return loginID == nil
     }
 }
