95a49e28bd6dc61f84293ba6cae61313b3a24dd8
ainopara <ainopara@gmail.com>
2016-05-18 12:56:31 +0800

    Make PullToActionController easier to understand.

--- Vendors/PullToActionViewController/PullToActionController.swift
+++ Vendors/PullToActionViewController/PullToActionController.swift
@@ -34,8 +34,8 @@ public class PullToActionController: NSObject, UIScrollViewDelegate {
     weak var scrollView: UIScrollView?
     weak var delegate: PullToActionDelagete?
 
-    var offset: CGPoint = CGPoint(x: 0, y: 0)
-    var size: CGSize = CGSize(width: 0, height: 0)
+    var offset: CGPoint = CGPoint(x: 0.0, y: 0.0)
+    var size: CGSize = CGSize(width: 0.0, height: 0.0)
     var inset: UIEdgeInsets = UIEdgeInsetsZero
     private var progressAction: [String: OffsetRange] = Dictionary<String, OffsetRange>()
 
@@ -43,7 +43,7 @@ public class PullToActionController: NSObject, UIScrollViewDelegate {
         self.scrollView = scrollView
         super.init()
 
-        scrollView.delegate = self
+        scrollView.delegate = self // TODO: forward message to original delegate of the scroll view.
         scrollView.addObserver(self, forKeyPath: "contentOffset", options: .New, context: nil)
         scrollView.addObserver(self, forKeyPath: "contentSize", options: .New, context: nil)
         scrollView.addObserver(self, forKeyPath: "contentInset", options: .New, context: nil)
@@ -54,6 +54,7 @@ public class PullToActionController: NSObject, UIScrollViewDelegate {
         self.scrollView?.removeObserver(self, forKeyPath: "contentSize")
         self.scrollView?.removeObserver(self, forKeyPath: "contentInset")
         self.scrollView?.delegate = nil
+
         DDLogDebug("[PullToAction] deinit")
     }
 
@@ -63,42 +64,41 @@ public class PullToActionController: NSObject, UIScrollViewDelegate {
 
     public override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
         if keyPath == "contentOffset" {
-            if let changes = change {
-                self.offset = changes["new"]?.CGPointValue ?? self.offset
-                if let delegateFunction = self.delegate?.scrollViewContentOffsetProgress {
-                    var progress: [String: Double] = Dictionary<String, Double>()
-                    for (name, actionOffset) in self.progressAction {
-                        let progressValue = actionOffset.progress(self.baseLineOffset(actionOffset.baseLine))
-                        progress.updateValue(progressValue, forKey: name)
-                    }
-                    delegateFunction(progress)
-                }
+            guard let changes = change, newOffsetValue = changes["new"] as? NSValue else { return }
+            self.offset = newOffsetValue.CGPointValue()
+
+            guard let delegateFunction = self.delegate?.scrollViewContentOffsetProgress else { return }
+            var progress = [String: Double]()
+            for (name, actionOffset) in self.progressAction {
+                let progressValue = actionOffset.progress(self.baseLineOffset(actionOffset.baseLine))
+                progress.updateValue(progressValue, forKey: name)
             }
-            //println("contentOffset: \(self.offset)")
+            delegateFunction(progress)
+            DDLogVerbose("[PullToAction] contentOffset: \(self.offset)")
         }
+
         if keyPath == "contentSize" {
-            if let changes = change {
-                self.size = changes["new"]?.CGSizeValue() ?? self.size
-                //println("size:w: \(self.size.width) h:\(self.size.height)")
-                self.delegate?.scrollViewContentSizeDidChange?(self.size)
-            }
+            guard let changes = change, newSizeValue = changes["new"] as? NSValue else { return }
+            self.size = newSizeValue.CGSizeValue()
+            DDLogVerbose("[PullToAction] contentSize:w: \(self.size.width) h:\(self.size.height)")
+            self.delegate?.scrollViewContentSizeDidChange?(self.size)
         }
+
         if keyPath == "contentInset" {
-            if let changes = change {
-                self.inset = changes["new"]?.UIEdgeInsetsValue() ?? self.inset
-                //println("inset: top: \(self.inset.top) bottom: \(self.inset.bottom)")
-            }
+            guard let changes = change, newInsetValue = changes["new"] as? NSValue else { return }
+            self.inset = newInsetValue.UIEdgeInsetsValue()
+            DDLogVerbose("[PullToAction] inset: top: \(self.inset.top) bottom: \(self.inset.bottom)")
         }
     }
 
     public func scrollViewDidEndDragging(scrollView: UIScrollView, willDecelerate decelerate: Bool) {
-        if self.offset.y < 0 {
+        if self.offset.y < 0.0 {
             DDLogDebug("[PullToAction] End dragging <- \(self.offset.y)")
             self.delegate?.scrollViewDidEndDraggingOutsideTopBoundWithOffset?(self.offset.y)
             return
         }
         let bottomOffset = self.offset.y + scrollView.bounds.height - self.size.height //TODO: consider content inset
-        if bottomOffset > 0 {
+        if bottomOffset > 0.0 {
             DDLogDebug("[PullToAction] End dragging -> \(bottomOffset)")
             self.delegate?.scrollViewDidEndDraggingOutsideBottomBoundWithOffset?(bottomOffset)
             return
@@ -106,21 +106,19 @@ public class PullToActionController: NSObject, UIScrollViewDelegate {
     }
 
     private func baseLineOffset(baseLine: OffsetBaseLine) -> Double {
-        guard let scrollView = self.scrollView else {
-            return Double(0)
-        }
+        guard let scrollView = self.scrollView else { return Double(0.0) }
         switch baseLine {
         case .Top:
             return Double(self.offset.y)
         case .Bottom:
             var temp = scrollView.bounds.height - self.size.height
-            if temp > 0 { temp = 0 }
+            if temp > 0.0 { temp = 0.0 }
             return Double(self.offset.y + temp)
         case .Left:
             return Double(self.offset.x)
         case .Right:
             var temp = scrollView.bounds.width - self.size.width
-            if temp > 0 { temp = 0 }
+            if temp > 0.0 { temp = 0.0 }
             return Double(self.offset.x + temp)
         }
     }
