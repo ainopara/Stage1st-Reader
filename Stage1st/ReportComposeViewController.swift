dcbf2e9fa2945b31917326025dc4f811abfbb2ba
ainopara <ainopara@gmail.com>
2016-08-21 18:35:17 +0800

    Remove spacing.

--- Stage1st/ReportComposeViewController.swift
+++ Stage1st/ReportComposeViewController.swift
@@ -57,7 +57,7 @@ final class ReportComposeViewController: UIViewController {
 
         super.init(nibName: nil, bundle: nil)
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
