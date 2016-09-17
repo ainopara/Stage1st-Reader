d4026335351569b057de7626064fb82538d14d18
ainopara <ainopara@gmail.com>
2016-09-17 20:29:47 +0800

    Cleaning.

--- Stage1st/S1CloudKitViewController.m
+++ Stage1st/S1CloudKitViewController.m
@@ -25,6 +25,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
     self.iCloudSwitch.on = [[NSUserDefaults standardUserDefaults] boolForKey:@"EnableSync"];
     NSError *error = [MyCloudKitManager lastCloudkitError];
     if (error) {
@@ -46,11 +47,6 @@
     
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 - (void)dealloc {
