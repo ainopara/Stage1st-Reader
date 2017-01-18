b9899ac283967b1350e35182f25d04a2843c0031
ainopara <ainopara@gmail.com>
2017-01-18 20:11:01 +0800

    [WIP] Avoid potential CI error.

--- Stage1st/DiscuzClient+Teleport.swift
+++ Stage1st/DiscuzClient+Teleport.swift
@@ -12,13 +12,13 @@ import JASON
 
 public let kStage1stDomain = "Stage1stDomain"
 
-struct DZError: Error {
-    enum Code: Int, _ErrorCodeProtocol {
-        public typealias _ErrorType = DZError
-
-        case loginFailedResponse
-    }
-}
+//struct DZError: Error {
+//    enum Code: Int, _ErrorCodeProtocol {
+//        public typealias _ErrorType = DZError
+//
+//        case loginFailedResponse
+//    }
+//}
 
 private func generateURLString(_ baseURLString: String, parameters: Parameters) -> String {
     let urlRequest = URLRequest(url: URL(string: baseURLString)!)
