a3e9d1fa9662c2c8f29810d0f378f3183a999544
ainopara <ainopara@gmail.com>
2017-02-19 16:03:24 +0800

    Add some localization strings. Convert strings file to UTF8 encoding.

--- Stage1st/AdvancedSettingsViewController.swift
+++ Stage1st/AdvancedSettingsViewController.swift
@@ -7,10 +7,10 @@
 //
 
 import QuickTableViewController
-
+// swiftlint:disable variable_name
 let ReverseActionKey = "Stage1st.Content.ReverseFloorAction"
 let HideStickTopicsKey = "Stage1st.TopicList.HideStickTopics"
-
+// swiftlint:enable variable_name
 final class AdvancedSettingsViewController: QuickTableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,23 +21,30 @@ final class AdvancedSettingsViewController: QuickTableViewController {
     func updateTable() {
         tableContents = [
             Section(title: "Content", rows: [
-                SwitchRow(title: "Reverse Floor Action", switchValue: UserDefaults.standard.bool(forKey: ReverseActionKey), action: { (row) in
+                SwitchRow(title: NSLocalizedString("AdvancedSettingsViewController.ReverseFloorActionRow.title", comment: ""),
+                          switchValue: UserDefaults.standard.bool(forKey: ReverseActionKey),
+                          action: { (row) in
                     UserDefaults.standard.set((row as! SwitchRow).switchValue, forKey: ReverseActionKey)
                 })
             ]),
             Section(title: "Topic List", rows: [
-                SwitchRow(title: "Hide Stick Topics", switchValue: UserDefaults.standard.bool(forKey: HideStickTopicsKey), action: { (row) in
+                SwitchRow(title: NSLocalizedString("AdvancedSettingsViewController.HideStickTopicsRow.title", comment: ""),
+                          switchValue: UserDefaults.standard.bool(forKey: HideStickTopicsKey),
+                          action: { (row) in
                     UserDefaults.standard.set((row as! SwitchRow).switchValue, forKey: HideStickTopicsKey)
                 })
             ]),
             Section(title: "Reset", rows: [
-                TapActionRow(title: "Reset Default Settings", action: resetDefaultSettings)
+                TapActionRow(title: NSLocalizedString("AdvancedSettingsViewController.ResetSettingsRow.title", comment: ""),
+                             action: resetDefaultSettings)
             ])
         ]
         tableView.reloadData()
     }
 
     private func resetDefaultSettings(_ row: Row) {
+        UserDefaults.standard.removeObject(forKey: ReverseActionKey)
+        UserDefaults.standard.removeObject(forKey: HideStickTopicsKey)
         updateTable()
     }
 }
