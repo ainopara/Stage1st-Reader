c271e045736468458b8d95a0fa2a4a973e6caebe
ainopara <ainopara@gmail.com>
2017-07-03 01:33:43 +0800

    `topic.authorUserID` is of `Optional<NSNumber>` type which should not be casted to `UInt` type directly.

--- Stage1st/PageRenderer.swift
+++ Stage1st/PageRenderer.swift
@@ -78,7 +78,7 @@ extension PageRenderer {
             var isFirstInPage = true
             var data = [[String: Any?]]()
             for floor in floors {
-                data.append(floorData(with: floor, topicAuthorID: topic.authorUserID as? UInt, isFirstInPage: isFirstInPage))
+                data.append(floorData(with: floor, topicAuthorID: topic.authorUserID.flatMap({ UInt($0) }), isFirstInPage: isFirstInPage))
                 isFirstInPage = false
             }
             return data
