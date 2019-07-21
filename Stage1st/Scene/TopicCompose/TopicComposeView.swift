32c47596bb3c88ead2701087093efa138afdc0bc
ainopara <ainopara@gmail.com>
2019-07-21 13:10:44 +0800

    Remove code related to SwiftUI since it can not be ignored by Xcode 10

--- Stage1st/Scene/TopicCompose/TopicComposeView.swift
+++ Stage1st/Scene/TopicCompose/TopicComposeView.swift
@@ -6,40 +6,40 @@
 //  Copyright © 2019 Renaissance. All rights reserved.
 //
 
-import SwiftUI
-
-@available(iOS 13.0, *)
-struct TopicComposeView: UIViewControllerRepresentable {
-
-    let title: String
-    let content: String
-
-    init(title: String = "", content: String = "") {
-        self.title = title
-        self.content = content
-    }
-
-    func makeUIViewController(context: UIViewControllerRepresentableContext<TopicComposeView>) -> UINavigationController {
-        return UINavigationController(rootViewController: TopicComposeViewController(forumID: 6))
-    }
-
-    func updateUIViewController(_ uiViewController: UINavigationController, context: UIViewControllerRepresentableContext<TopicComposeView>) {
-        let composeViewController = uiViewController.viewControllers.first! as! TopicComposeViewController
-        composeViewController.textField.text = title
-        composeViewController.textView.text = content
-    }
-
-}
-
-#if DEBUG
-@available(iOS 13.0, *)
-struct TopicComposeView_Previews: PreviewProvider {
-    static var previews: some View {
-        Group {
-            TopicComposeView()
-            TopicComposeView(title: "Hello World")
-            TopicComposeView(title: "Hello World", content: "WWWWWWWWWWWWWWWWWWWWWWWWWWWW")
-        }
-    }
-}
-#endif
+//import SwiftUI
+//
+//@available(iOS 13.0, *)
+//struct TopicComposeView: UIViewControllerRepresentable {
+//
+//    let title: String
+//    let content: String
+//
+//    init(title: String = "", content: String = "") {
+//        self.title = title
+//        self.content = content
+//    }
+//
+//    func makeUIViewController(context: UIViewControllerRepresentableContext<TopicComposeView>) -> UINavigationController {
+//        return UINavigationController(rootViewController: TopicComposeViewController(forumID: 6))
+//    }
+//
+//    func updateUIViewController(_ uiViewController: UINavigationController, context: UIViewControllerRepresentableContext<TopicComposeView>) {
+//        let composeViewController = uiViewController.viewControllers.first! as! TopicComposeViewController
+//        composeViewController.textField.text = title
+//        composeViewController.textView.text = content
+//    }
+//
+//}
+//
+//#if DEBUG
+//@available(iOS 13.0, *)
+//struct TopicComposeView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        Group {
+//            TopicComposeView()
+//            TopicComposeView(title: "Hello World")
+//            TopicComposeView(title: "Hello World", content: "WWWWWWWWWWWWWWWWWWWWWWWWWWWW")
+//        }
+//    }
+//}
+//#endif
