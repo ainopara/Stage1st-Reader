061158b8e6997b6a66c3a08db4066451b0ade1ef
ainopara <ainopara@gmail.com>
2019-10-06 12:34:46 +0800

    Add SettingsView

---
+++ Stage1st/Scene/Settings/SettingsView.swift
@@ -0,0 +1,28 @@
+//
+//  SettingsView.swift
+//  Stage1st
+//
+//  Created by Zheng Li on 2019/10/6.
+//  Copyright © 2019 Renaissance. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+struct SettingsView: View {
+    var body: some View {
+        Form {
+            Section {
+                Button(action: {}) {
+                    Text("Login")
+                }
+            }
+        }
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
